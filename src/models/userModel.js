245e7eac3f1c06e7e2f50f9b72434fdc680cc8ba
Ethan <ethanbone435@gmail.com>
2024-07-31 13:45:35 +0200

    added name and suername to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -49,6 +49,8 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema(
     {
         username: { type: String, required: true },
+        first_name: { type: String, required: true },
+        surname: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         score: { type: Number, default: 0 },
